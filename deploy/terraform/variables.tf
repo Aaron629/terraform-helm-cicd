7ed487524b8e732405dfa59ba58f9a3388de6937
aaron <aaronhsu629@aaron.localdomain>
2025-09-02 01:48:07 +0800

    fix:create the kinD cluster

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,7 +1,7 @@
 # 供 providers.tf 使用（KinD 方案：workflow 以 -var 傳入）
 variable "kubeconfig_path" {
   type    = string
-  default = "~/.kube/config"
+  default = "/home/runner/.kube/config"  # 在 GitHub Actions 中的實際路徑
 }
 
 variable "kube_context" {
